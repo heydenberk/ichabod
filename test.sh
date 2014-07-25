a383a110c92d21aab675dc58c7221b1c6a2a9f85
Dave Berton <dberton@monetate.com>
2014-07-25 14:36:38 -0400

    bump wkhtmltopdf

--- test.sh
+++ test.sh
@@ -14,8 +14,12 @@ function cleanup()
 {
     rm -f $HELLO_FILE
     rm -f $ANIM_FILE
-    sleep 1
-    kill $ichabod_pid > /dev/null 2>&1
+    while sleep 1
+          echo Killing ichabod on port $PORT
+          kill -0 $ichabod_pid >/dev/null 2>&1
+    do
+        kill $ichabod_pid > /dev/null 2>&1
+    done
 }
 
 function die()
@@ -111,7 +115,13 @@ function test004()
 
     # simple image
     HELLO=$(curl -s -X POST http://localhost:$PORT/003 --data "html=<html><head></head><body style='background-color: red;'><div style='background-color: blue; color: white;'>helloworld</div></body></html>&width=100&height=100&format=png&output=$HELLO_FILE")
-    HELLO_EXPECTED='{"path": "'$HELLO_FILE'", "result": , "warnings": []}'
+    HELLO_EXPECTED=$(printf "{
+   \"conversion\" : true,
+   \"errors\" : null,
+   \"path\" : \"$HELLO_FILE\",
+   \"result\" : null,
+   \"warnings\" : null
+}")
     if [ "$HELLO" != "$HELLO_EXPECTED" ]; then
         die "Invalid hello world result: [$HELLO] expected: [$HELLO_EXPECTED]"
     fi
@@ -121,7 +131,13 @@ function test004()
 
     # animated image
     ANIM=$(curl -s -X POST http://localhost:$PORT/003 --data "html=<html><head></head><body style='background-color: red;'><div id='word' style='background-color: blue; color: darkgrey;'>hello</div></body></html>&width=100&height=100&format=gif&output=$ANIM_FILE&js=(function(){ichabod.setTransparent(0); ichabod.snapshotPage(); document.getElementById('word').innerHTML='world'; ichabod.snapshotPage(); ichabod.saveToOutput(); return 42;})();")
-    ANIM_EXPECTED='{"path": "'$ANIM_FILE'", "result": 42, "warnings": []}'
+    ANIM_EXPECTED=$(printf "{
+   \"conversion\" : true,
+   \"errors\" : null,
+   \"path\" : \"$ANIM_FILE\",
+   \"result\" : 42,
+   \"warnings\" : null
+}")
     if [ "$ANIM" != "$ANIM_EXPECTED" ]; then
         die "Invalid animated result: [$ANIM] expected: [$ANIM_EXPECTED]"
     fi
@@ -132,7 +148,7 @@ function test004()
 
 test001
 test002
-test003
+#test003
 test004
 cleanup
 echo "Testing successful."
